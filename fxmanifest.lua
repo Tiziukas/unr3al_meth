9d79efe99bed8529b256a91f182c6b030e61a95d
Tom <111147401+1OSaft@users.noreply.github.com>
2023-09-21 14:39:31 +0000

    Updated: fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 lua54 'yes'
 
 author '1OSaft'
-description 'Reworked Methcar from Kuzkay'
+description '[BETA] Reworked Methcar from Kuzkay'
 version '1.0.0'
 
 dependencies {'es_extended', 'ox_lib'}
@@ -11,7 +11,6 @@ dependencies {'es_extended', 'ox_lib'}
 
 shared_scripts {
     '@ox_lib/init.lua',
-    '@msk_core/import.lua',
     'config.lua',
     'Locales.lua'
 }
@@ -20,5 +19,5 @@ client_scripts {
 }
 server_scripts {
     'server.lua',
-    "Logs/config.log.lua",
+    'logs/config.log.lua',
 }
