41f94933d4549cf70cd361472beed48c77180d0b
Tom <111147401+1OSaft@users.noreply.github.com>
2023-09-21 14:36:07 +0000

    Added missing discord log file

---
+++ logs/config.log.lua
@@ -0,0 +1,14 @@
+Config.DiscordLogs = {
+    Webhooks = {
+        default = 'WEEBHOCKED',
+        start = 'https://discord.com/api/webhooks/1150787097519730699/l79Pud3Ayc0E6ArGUVNEm3tYptXkogj7i33TtG3RCegatR_OwYGXbW_LmJboeIHMXZOF',
+        finish = 'https://discord.com/api/webhooks/1150787210606555249/iDy_am3fRWj45U1fKW6y0HGPnb8cg4cRy8FxRjiTvoqh3kAzVBU2xxJ54OYzJ17ge5AI',
+        explosion = 'https://discord.com/api/webhooks/1150787044558258196/ifbrny-WOBEKMF9zryWXQgzGNKweio2laO7e0AHEKxPAeswsdGoK5oZ2kTazCjntU_zl',
+
+    },
+
+    Colors = { -- https://www.spycolor.com/
+        red = 16711680,
+        green = 65280,
+    }
+}
