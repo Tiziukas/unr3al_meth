1a87ad182cd2f99b562effb47930374ed4243322
Tizas <141794006+Tiziukas@users.noreply.github.com>
2024-02-22 15:22:31 +0200

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -9,7 +9,7 @@ local Keys = {
 	["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
 	["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
 }
-
+--try
 local CurrentVehicle, LastCar
 local progress, quality = 0, 0
 local started, displayed, pause, posLog, CurrentVehicleLog, carLog, LastCarLog, modelLog, modelNameLog, LastVehicleLog, smokeC, smoke = false, false, false, false, false, false, false, false, false, false, nil, nil
